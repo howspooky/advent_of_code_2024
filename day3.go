2f157dd4225796d60b13246b9a3fb50d3d3a1b75
HowSpooky <25248588+HowSpooky@users.noreply.github.com>
2024-12-03 15:59:18 +0000

    feat: day 3 solution

---
+++ day3.go
@@ -0,0 +1,77 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"regexp"
+	"strconv"
+)
+
+var MUL = 0
+var DO = 1
+var DONT = 2
+
+func day3() {
+	input, err := os.Open("inputs/day3.txt")
+	if err != nil {
+		panic(fmt.Errorf("failed to open input file: %w", err))
+	}
+	defer input.Close()
+
+	regex := regexp.MustCompile(`mul\((\d+),(\d+)\)|do\(\)|don't\(\)`)
+	scanner := bufio.NewScanner(input)
+
+	var instructions [][]int
+
+	for scanner.Scan() {
+		line := scanner.Text()
+		matches := regex.FindAllStringSubmatch(line, -1)
+		for _, match := range matches {
+			if match[0] == "do()" {
+				instructions = append(instructions, []int{DO})
+			} else if match[0] == "don't()" {
+				instructions = append(instructions, []int{DONT})
+			} else {
+				num1, err := strconv.Atoi(match[1])
+				if err != nil {
+					panic(err)
+				}
+				num2, err := strconv.Atoi(match[2])
+				if err != nil {
+					panic(err)
+				}
+				instructions = append(instructions, []int{MUL, num1, num2})
+			}
+		}
+	}
+
+	if err := scanner.Err(); err != nil {
+		panic(fmt.Errorf("error reading input file: %w", err))
+	}
+
+	part1 := 0
+	for i := 0; i < len(instructions); i++ {
+		instr := instructions[i]
+		if instr[0] == MUL {
+			part1 += instr[1] * instr[2]
+		}
+	}
+	println("part1: ", part1) // 174960292
+
+	enabled := true
+	part2 := 0
+	for i := 0; i < len(instructions); i++ {
+		instr := instructions[i]
+		if instr[0] == DO {
+			enabled = true
+		} else if instr[0] == DONT {
+			enabled = false
+		} else if instr[0] == MUL {
+			if enabled {
+				part2 += instr[1] * instr[2]
+			}
+		}
+	}
+	println("part2: ", part2) // 56275602
+}
